d51b2a2889098f5e59739eb8fca53a3ca32e1175
Pierre Seznec <pierre@yesorno.bet>
2022-05-28 16:35:16 +0200

    remove unused constants

--- src/constants.ts
+++ src/constants.ts
@@ -13,13 +13,3 @@ export const raceFees = {
   [raceClass.CLASS_2]: 190,
   [raceClass.CLASS_1]: 250,
 };
-
-export const gains = {
-  [raceClass.CLASS_5]: [286, 189, 3, 126, 5, 42, 0, 0, 0, 0, 0, 0],
-  [raceClass.CLASS_4]: [0, 0, 240, 180, 120, 6, 0, 0, 0, 0, 0, 0],
-  [raceClass.CLASS_3]: [0, 0, 324, 180, 120, 81, 0, 0, 0, 0, 0, 0],
-  [raceClass.CLASS_2]: [190],
-  [raceClass.CLASS_1]: [250],
-};
-
-export const mmrGains = [30, 2, 15, 10, 10, 10, 10, -10, -10, 0, -30, -40];
